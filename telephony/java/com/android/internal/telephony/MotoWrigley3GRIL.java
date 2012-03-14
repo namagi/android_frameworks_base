be2dd69a56bde6fb6f3b24919357eebf692690b7
nadlabak <pavel@doshaska.net>
2012-03-14 01:50:57 +0100

    MotoWrigley3GRIL: adapt to ics, fix outgoing call identification dependency on CallerID setting
    Change-Id: I8cf835d07c88d06063fbff1e9f1baab360713646

--- telephony/java/com/android/internal/telephony/MotoWrigley3GRIL.java
+++ telephony/java/com/android/internal/telephony/MotoWrigley3GRIL.java
@@ -18,12 +18,13 @@ package com.android.internal.telephony;
 
 import android.content.Context;
 import android.os.Parcel;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 
-import com.android.internal.telephony.gsm.NetworkInfo;
 import com.android.internal.telephony.gsm.SuppServiceNotification;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * RIL class for Motorola Wrigley 3G RILs which need
@@ -32,9 +33,6 @@ import java.util.ArrayList;
  * {@hide}
  */
 public class MotoWrigley3GRIL extends RIL {
-    public MotoWrigley3GRIL(Context context) {
-        super(context);
-    }
 
     public MotoWrigley3GRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
@@ -58,12 +56,82 @@ public class MotoWrigley3GRIL extends RIL {
          * It never sends 10 where it would be appropriate, so it's safe
          * to just convert every occurence of 10 to 0.
          */
-        if (notification.notificationType == SuppServiceNotification.NOTIFICATION_TYPE_MT) {
+        if (notification.notificationType == 1) {
             if (notification.code == SuppServiceNotification.MT_CODE_ADDITIONAL_CALL_FORWARDED) {
                 notification.code = SuppServiceNotification.MT_CODE_FORWARDED_CALL;
             }
         }
 
         return notification;
+    }
+
+    @Override
+    protected Object
+    responseCallList(Parcel p) {
+        int num;
+        int voiceSettings;
+        ArrayList<DriverCall> response;
+        DriverCall dc;
+
+        num = p.readInt();
+        response = new ArrayList<DriverCall>(num);
+
+        for (int i = 0 ; i < num ; i++) {
+            dc = new DriverCall();
+
+            dc.state = DriverCall.stateFromCLCC(p.readInt());
+            dc.index = p.readInt();
+            dc.TOA = p.readInt();
+            dc.isMpty = (0 != p.readInt());
+            dc.isMT = (0 != p.readInt());
+            dc.als = p.readInt();
+            voiceSettings = p.readInt();
+            dc.isVoice = (0 == voiceSettings) ? false : true;
+            dc.isVoicePrivacy = (0 != p.readInt());
+            dc.number = p.readString();
+            int np = p.readInt();
+            /** numberPresentation needs to be overriden for outgoing calls
+                in case of Moto Wrigley3G RIL under ICS, to prevent outgoing calls
+                to be identified as "Unknown" on InCallScreen and in the call logs
+                when CallerID option is set to "Network default" or "Hide number". **/
+            if (!dc.isMT) np = 0;
+            dc.numberPresentation = DriverCall.presentationFromCLIP(np);
+            dc.name = p.readString();
+            dc.namePresentation = p.readInt();
+            int uusInfoPresent = p.readInt();
+            if (uusInfoPresent == 1) {
+                dc.uusInfo = new UUSInfo();
+                dc.uusInfo.setType(p.readInt());
+                dc.uusInfo.setDcs(p.readInt());
+                byte[] userData = p.createByteArray();
+                dc.uusInfo.setUserData(userData);
+                riljLogv(String.format("Incoming UUS : type=%d, dcs=%d, length=%d",
+                                dc.uusInfo.getType(), dc.uusInfo.getDcs(),
+                                dc.uusInfo.getUserData().length));
+                riljLogv("Incoming UUS : data (string)="
+                        + new String(dc.uusInfo.getUserData()));
+                riljLogv("Incoming UUS : data (hex): "
+                        + IccUtils.bytesToHexString(dc.uusInfo.getUserData()));
+            } else {
+                riljLogv("Incoming UUS : NOT present!");
+            }
+
+            // Make sure there's a leading + on addresses with a TOA of 145
+            dc.number = PhoneNumberUtils.stringFromStringAndTOA(dc.number, dc.TOA);
+
+            response.add(dc);
+
+            if (dc.isVoicePrivacy) {
+                mVoicePrivacyOnRegistrants.notifyRegistrants();
+                riljLog("InCall VoicePrivacy is enabled");
+            } else {
+                mVoicePrivacyOffRegistrants.notifyRegistrants();
+                riljLog("InCall VoicePrivacy is disabled");
+            }
+        }
+
+        Collections.sort(response);
+
+        return response;
     }
 }
