32d2a671e7b6a2de674e096fbc748d9174f6fe53
nadlabak <pavel@doshaska.net>
2012-11-21 22:20:41 +0100

    remove the remains of 'Externalize symbol picker options...'
    Change-Id: I22012384e53182036bc05b2fdad4eb4930bec509

--- core/java/android/text/method/QwertyKeyListener.java
+++ core/java/android/text/method/QwertyKeyListener.java
@@ -789,8 +789,8 @@ public class QwertyKeyListener extends BaseKeyListener {
                         Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
     }
 
-    private static SparseArray<Integer> SYM_PICKER_RES_ID =
-                        new SparseArray<Integer>();
+    private static SparseArray<String> PICKER_SETS =
+                        new SparseArray<String>();
 
     static {
         PICKER_SETS.put('A', "\u00C0\u00C1\u00C2\u00C4\u00C6\u00C3\u00C5\u0104\u0100");
@@ -858,13 +858,10 @@ public class QwertyKeyListener extends BaseKeyListener {
 
     private boolean showCharacterPicker(View view, Editable content, char c,
                                         boolean insert, int count) {
-        Integer resId = SYM_PICKER_RES_ID.get(c);
-
-        if (resId == null) {
+        String set = PICKER_SETS.get(c);
+        if (set == null) {
             return false;
         }
-
-        String set = view.getContext().getString(resId);
 
         if (count == 1) {
             if (set.length() == 1) {
