e4889a1e9dfd3ce22858828e5b2e5f1999cf17f3
nadlabak <pavel@doshaska.net>
2012-12-25 14:26:57 +0100

    LegacyUSBDeviceManager: reintroduce persist.service.adb.enable
    Change-Id: If8aee51149d0ebdada635dbb77ba3024d828c7aa

--- services/java/com/android/server/usb/LegacyUsbDeviceManager.java
+++ services/java/com/android/server/usb/LegacyUsbDeviceManager.java
@@ -112,6 +112,7 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
         public void onChange(boolean selfChange) {
             boolean enable = (Settings.Secure.getInt(mContentResolver,
                     Settings.Secure.ADB_ENABLED, 0) > 0);
+            SystemProperties.set("persist.service.adb.enable", enable ? "1" : "0");
             mHandler.sendMessage(MSG_ENABLE_ADB, enable);
         }
     }
