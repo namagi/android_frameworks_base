1f0df2d344b4382e9e4898cf553e6a2a1caf3a4c
namagi <br.mladen@gmail.com>
2014-01-29 06:41:16 +0100

    Reduce max hidden processes to 10
    Change-Id: Ib4f657af7bac2964dba9023cbd48e545c22fc0ea

--- services/java/com/android/server/am/ProcessList.java
+++ services/java/com/android/server/am/ProcessList.java
@@ -100,7 +100,7 @@ class ProcessList {
     // The maximum number of hidden processes we will keep around before
     // killing them; this is just a control to not let us go too crazy with
     // keeping around processes on devices with large amounts of RAM.
-    static final int MAX_HIDDEN_APPS = 15;
+    static final int MAX_HIDDEN_APPS = 10;
 
     // We put empty content processes after any hidden processes that have
     // been idle for less than 15 seconds.
