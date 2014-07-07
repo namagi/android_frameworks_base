0d280eadc61f8540cc53d67bb35922d41be7c06d
namagi <br.mladen@gmail.com>
2014-07-07 19:48:30 +0200

    Revert "SystemUI: prevent NPE on clear recents double press"
    This reverts commit 6c38cac1123e632bd14968ab1ce174a67df6954d.

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -463,7 +463,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 @Override
                 public void onClick(View v) {
                     mRecentsContainer.removeAllViewsInLayout();
-                    mClearRecents.setVisibility(View.INVISIBLE);
                 }
             });
         }
